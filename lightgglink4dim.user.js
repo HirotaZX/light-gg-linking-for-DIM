3b2b916cbf8c496990d31e468f079a0e7b0e9344
HZDeluxe <55805734+HZDeluxe@users.noreply.github.com>
2021-09-22 07:56:47 +0800

    Fix script not working

--- lightgglink4dim.user.js
+++ lightgglink4dim.user.js
@@ -13,29 +13,41 @@
 // @grant               none
 // ==/UserScript==
 
-(function() {
+(function () {
     'use strict';
 
     let targetNode = null;
-    const waitNode = function() {
+    const waitNode = function () {
         targetNode = document.querySelector('#app > div:first-child > div:first-child');
         if (!targetNode) {
             setTimeout(waitNode, 1000);
         } else {
-            const observer = new MutationObserver(function(mutationsList) {
-                for(const mutation of mutationsList) {
+            const observer = new MutationObserver(function (mutationsList) {
+                for (const mutation of mutationsList) {
                     if (mutation.type === 'childList'
-                       && mutation.addedNodes.length != 0
-                       && mutation.addedNodes[0].classList.contains('item-popup')) {
+                        && mutation.addedNodes.length != 0
+                        && mutation.addedNodes[0].classList.contains('item-popup')) {
                         const itemPopup = mutation.addedNodes[0];
-                        const addLgg = function() {
+                        const addLgg = function () {
                             if (itemPopup.querySelector('.btn-lgg')) {
                                 return;
                             }
-                            const itemNameElm = itemPopup.querySelector('a[href^="https://destinytracker.com/"]');
-                            const itemId = itemNameElm.href.match(/\/items\/(\d+)\?/i)[1];
+                            let item = null;
+                            const itemNameElm = itemPopup.querySelector('a');
+                            if (itemNameElm && itemNameElm.parentElement) {
+                                const fiberKey = Object.keys(itemNameElm.parentElement).find(key => key.startsWith("__reactFiber$"));
+                                if (fiberKey && itemNameElm.parentElement[fiberKey]) {
+                                    const fiber = itemNameElm.parentElement[fiberKey];
+                                    if (fiber && fiber.return && fiber.return.memoizedProps && fiber.return.memoizedProps.item) {
+                                        item = fiber.return.memoizedProps.item;
+                                    }
+                                }
+                            }
+                            if (!item || item.name.toUpperCase() != itemNameElm.innerText.toUpperCase()) {
+                                return;
+                            }
                             const tempDiv = document.createElement('div');
-                            tempDiv.innerHTML = '<a class="btn-lgg" style="float:right;margin-right:5px;" target="_blank" href="https://www.light.gg/db/items/' + itemId + '/"><img src="data:image/webp;base64,UklGRlwBAABXRUJQVlA4TE8BAAAvD8ADEO/BoJEkRX34KOX9C2P+tsE4kmwl8z8uJxLwHLiRfzruj2XQSJKivrt//8pewEtgZmqDbCP1HvrV3uOFLpQFChQckAqUwh/x4T+AoAHB6pEjgUSBTCB7YIF8DCc/0DbnHtuh4H2xH7vzuoTgEPzv2JLPPp99plGvRrqPrECCi5IAdH89/C34Hwh+DxWQfH5OoJ/VWBdqjmzoS8wTcOgGKBxGkm1a59nWt2375x/YUwp3343ov8K2bZvsdFdnxKFAAFRXBiCEMSPA6fYHu9MqIYCJQGs07nVm2/t1Ssh9whyybKTbxzSfHTGF9G/DEWyvCaE7dqD9n5w1yG4roN8j/f96M/7XsTlAHbaN3/P9AeM/c3n2vf3crBY6bmoarG5ZzZLkug0qDsrRTB/blOwChyKY6fW+XjBe5JRzTFaHS8OLHKMCZ8lveIFzBAA=" /></a>';
+                            tempDiv.innerHTML = '<a class="btn-lgg" style="float:right;margin-right:5px;" target="_blank" href="https://www.light.gg/db/items/' + item.hash + '/"><img src="data:image/webp;base64,UklGRlwBAABXRUJQVlA4TE8BAAAvD8ADEO/BoJEkRX34KOX9C2P+tsE4kmwl8z8uJxLwHLiRfzruj2XQSJKivrt//8pewEtgZmqDbCP1HvrV3uOFLpQFChQckAqUwh/x4T+AoAHB6pEjgUSBTCB7YIF8DCc/0DbnHtuh4H2xH7vzuoTgEPzv2JLPPp99plGvRrqPrECCi5IAdH89/C34Hwh+DxWQfH5OoJ/VWBdqjmzoS8wTcOgGKBxGkm1a59nWt2375x/YUwp3343ov8K2bZvsdFdnxKFAAFRXBiCEMSPA6fYHu9MqIYCJQGs07nVm2/t1Ssh9whyybKTbxzSfHTGF9G/DEWyvCaE7dqD9n5w1yG4roN8j/f96M/7XsTlAHbaN3/P9AeM/c3n2vf3crBY6bmoarG5ZzZLkug0qDsrRTB/blOwChyKY6fW+XjBe5JRzTFaHS8OLHKMCZ8lveIFzBAA=" /></a>';
                             itemNameElm.after(tempDiv.firstChild);
                         };
                         addLgg();
